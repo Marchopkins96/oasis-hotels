3a1f342968e4e328570edcf8bee45643305e565a
Marc Hopkins <marchopkins1@outlook.com>
2024-04-12 10:12:54 +0000

    Add Booking model

--- hotel_bookings/models.py
+++ hotel_bookings/models.py
@@ -17,4 +17,38 @@ class Extras(models.Model):
     def __str__(self):
         return self.name
 
+class Hotel(models.Model):
+    """
+    Add a new hotel as Admin in admin panel
+    """
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    image = CloudinaryField('log_hotel_images')
+    max_guests = models.PositiveIntegerField(default=1)
+    rooms = models.PositiveIntegerField(default=1)
+    extras = models.ManyToManyField(Amenity)
 
+    def __str__(self):
+        return self.name
+
+class Booking(models.Model):
+    """
+    Hotel Booking
+    This class handles the information related to a booking made by a user
+    for a specific hotel. It stores details such as the booked hotel,
+    the user who made the booking, check-in and check-out dates,
+    the number of guests, optional quantities of breakfasts and kids club tickets
+
+    """
+    hotel = models.ForeignKey(Hotel, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    check_in_date = models.DateField()
+    check_out_date = models.DateField()
+    num_guests = models.PositiveIntegerField()
+    breakfast_included = models.PositiveIntegerField(default=0, null=True)
+    kids_club_tickets = models.DecimalField(
+                  max_digits=8, decimal_places=2, default=0)
+
+    def __str__(self):
+        return f"Booking {self.id} - Hotel: {self.hotel.name}, User: {self.user.username}"  # noqa
