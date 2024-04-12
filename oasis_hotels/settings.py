4dbeed8c77a1489195f4bd16ea01d80f1c503b99
Marc Hopkins <marchopkins1@outlook.com>
2024-04-12 09:46:34 +0000

    Update SECRET_KEY:

--- oasis_hotels/settings.py
+++ oasis_hotels/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d4e_zqd$y6c^wq#(@%$3q8u4i(^(2f!@=!@a0($-(7z)06-#bt'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
