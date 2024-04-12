5431465ce0506ab546ab83db94e5d6a4e215ec28
Marc Hopkins <marchopkins1@outlook.com>
2024-04-12 10:03:11 +0000

    Add Extras model

--- hotel_bookings/models.py
+++ hotel_bookings/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+from django.core.exceptions import ValidationError
+from django.utils import timezone
 
 # Create your models here.
+
+class Extras(models.Model):
+    """
+    Add a new Extra as Admin in admin panel
+    """
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+
+    def __str__(self):
+        return self.name
