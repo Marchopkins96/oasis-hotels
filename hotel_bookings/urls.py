0b13bd958d896e55e38dac2b003316a88f0c2052
Marc Hopkins <marchopkins1@outlook.com>
2024-04-12 14:42:26 +0000

    Add django allauth settings

--- hotel_bookings/urls.py
+++ hotel_bookings/urls.py
@@ -1,2 +1,7 @@
 from . import views
 from django.urls import path
+
+urlpatterns = [
+    # Open Home page
+    path('', views.hotel_list, name='home')
+]
--- hotel_bookings/views.py
+++ hotel_bookings/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, reverse, redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
+from datetime import date
+from django.utils import timezone
+import json
+from django.views import generic, View
+from .forms import BookingForm
+from .models import Hotel, Booking, Extras
 
 # Create your views here.
 
-def hotel_bookings(request):
-    return HttpResponse("Hello, World!")
