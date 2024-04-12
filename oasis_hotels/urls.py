abe8ff09cc922b898bdfc27e2a74c6754264587f
Marc Hopkins <marchopkins1@outlook.com>
2024-04-12 09:00:58 +0000

    Add url link to hotel_bookings app

--- oasis_hotels/urls.py
+++ oasis_hotels/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from hotel_bookings.views import hotel_bookings
 
 urlpatterns = [
+    path('hotel_bookings/', hotel_bookings, name='hotel_bookings'),
     path('admin/', admin.site.urls),
 ]
