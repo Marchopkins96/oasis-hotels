721a4032e1d9246507cfe036ba12aa32aa74f431
Marc Hopkins <marchopkins1@outlook.com>
2024-04-14 17:02:52 +0000

    Create booking_create class

--- hotel_bookings/views.py
+++ hotel_bookings/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, reverse, redirect
 from django.core.paginator import Paginator
 from django.views import generic, View
+from .forms import BookingForm
 from .models import Hotel
 
 class Home(generic.TemplateView):
@@ -18,5 +19,22 @@ def hotel_booking(request):
     hotels = paginator.get_page(page_number)
 
     return render(request, 'hotel_booking.html', {'hotels': hotels})
+
+def booking_create(request, hotel_id):
+    hotel = Hotel.objects.get(id=hotel_id)
+
+    if request.method == 'POST':
+        form = BookingForm(request.POST)
+        if form.is_valid():
+            booking = form.save(commit=False)
+            booking.hotel = hotel
+            booking.user = request.user
+            booking.save()
+            return redirect('booking_success')
+    else:
+        form = BookingForm()
+
+    context = {'hotel': hotel, 'form': form}
+    return render(request, 'booking.html', context)
 
 
