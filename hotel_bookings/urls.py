58bf6bcec6c9f2782384471e4d15f25301e544c8
Marc Hopkins <marchopkins1@outlook.com>
2024-04-14 10:40:25 +0000

    Add path and views for hotel_booking.html

--- hotel_bookings/urls.py
+++ hotel_bookings/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.hotel_list, name='home'),
-    path('hotel-booking/', hotel_booking, name='hotel_booking'),
+    path('hotel-booking/', views.hotel_booking, name='hotel_booking'),
 ]
--- hotel_bookings/views.py
+++ hotel_bookings/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404, reverse, redirect
+from django.core.paginator import Paginator
 from django.views import generic, View
 from .models import Hotel
 
@@ -9,4 +10,12 @@ class Home(generic.TemplateView):
 def hotel_list(request):
     hotels = Hotel.objects.all()
     return render(request, 'index.html', {'hotels': hotels})
+
+def hotel_booking(request):
+    all_hotels = Hotel.objects.all()
+    paginator = Paginator(all_hotels, 6)  # Display 6 cabins per page
+    page_number = request.GET.get('page')
+    hotels = paginator.get_page(page_number)
+
+    return render(request, 'hotel_booking.html', {'hotels': hotels})
 
