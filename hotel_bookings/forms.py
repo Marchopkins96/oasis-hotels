260c3c4d3d0fafde6600f5b37f78caeabdf00a5c
Marc Hopkins <marchopkins1@outlook.com>
2024-04-22 13:45:28 +0000

    Update forms.py with extras fields

--- hotel_bookings/forms.py
+++ hotel_bookings/forms.py
@@ -1,11 +1,17 @@
 from django import forms
-from .models import Booking
+from .models import Booking, Extras
 from django.core.validators import MinValueValidator
 
 
 class BookingForm(forms.ModelForm):
     num_guests = forms.IntegerField(validators=[MinValueValidator(1)])
+    breakfast_included = forms.IntegerField(validators=[MinValueValidator(0)], required=False)
+    kids_club_tickets = forms.IntegerField(validators=[MinValueValidator(0)], required=False)
 
     class Meta:
         model = Booking
-        fields = ['check_in_date', 'check_out_date', 'num_guests']
+        fields = ['check_in_date',
+                  'check_out_date',
+                  'num_guests',
+                  'breakfast_included',
+                  'kids_club_tickets']
