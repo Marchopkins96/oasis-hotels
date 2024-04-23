cf9c9be450574e721479ae546b122d7f738fd9d2
Marc Hopkins <marchopkins1@outlook.com>
2024-04-23 15:48:41 +0000

    Update models.py

--- hotel_bookings/models.py
+++ hotel_bookings/models.py
@@ -12,7 +12,6 @@ class Extras(models.Model):
     Add a new Extra as Admin in admin panel
     """
     name = models.CharField(max_length=100)
-    price = models.DecimalField(max_digits=8, decimal_places=2)
 
     def __str__(self):
         return self.name
@@ -23,7 +22,6 @@ class Hotel(models.Model):
     """
     name = models.CharField(max_length=100)
     description = models.TextField()
-    price = models.DecimalField(max_digits=8, decimal_places=2)
     image = CloudinaryField('log_hotel_images')
     max_guests = models.PositiveIntegerField(default=1)
     rooms = models.PositiveIntegerField(default=1)
@@ -48,8 +46,6 @@ class Booking(models.Model):
     num_guests = models.PositiveIntegerField(default=1)
     breakfast_included = models.PositiveIntegerField(default=0, null=True)
     kids_club_tickets = models.PositiveIntegerField(default=0, null=True)
-    total_price = models.DecimalField(
-                  max_digits=8, decimal_places=2, default=0)
 
     def __str__(self):
         return f"Booking {self.id} - Hotel: {self.hotel.name}, User: {self.user.username}"  # noqa
