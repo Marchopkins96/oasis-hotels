506a9ed0123439e4ef7dfc2592853929e536fb6d
Marc Hopkins <marchopkins1@outlook.com>
2024-04-26 13:27:05 +0000

    Add 404 error page with view/url

--- hotel_bookings/urls.py
+++ hotel_bookings/urls.py
@@ -1,13 +1,21 @@
 from . import views
 from django.urls import path
+from django.conf.urls import handler404
+from .views import custom_handler404
 
+#URLs list
 urlpatterns = [
+    # Open Home and Contact page
     path('', views.hotel_list, name='home'),
-    path('hotel-booking/', views.hotel_booking, name='hotel_booking'),
-    path('booking/<int:hotel_id>/', views.booking_create, name='my_booking'),  # noqa
-    path('booking/success/<int:hotel_id>/<int:booking_id>/', views.booking_success, name='booking_success'),  # noqa
-    path('booking-overview/', views.booking_overview, name='booking_overview'),
-    path('booking/edit/<int:booking_id>/', views.edit_booking, name='edit_booking'), # noqa
-    path('booking/<int:booking_id>/delete/', views.delete_booking, name='delete_booking'),  # noqa
     path('contact/', views.contact_view, name='contact'),
+
+    # Create, Read, Update and Delete Cabin Bookings
+    path('hotel-booking/', views.hotel_booking, name='hotel_booking'),
+    path('booking/<int:hotel_id>/', views.booking_create, name='my_booking'),
+    path('booking/success/<int:hotel_id>/<int:booking_id>/', views.booking_success, name='booking_success'),
+    path('booking-overview/', views.booking_overview, name='booking_overview'),
+    path('booking/edit/<int:booking_id>/', views.edit_booking, name='edit_booking'),
+    path('booking/<int:booking_id>/delete/', views.delete_booking, name='delete_booking'),
 ]
+
+handler404 = custom_handler404
